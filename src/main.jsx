82dbfcad819dec2b2d8b3ba6bb53d1f1dfaac69c
Ramiro Gutierrez Paulos <ramagp00@gmail.com>
2023-09-05 23:13:49 -0300

    Store added

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,17 @@ import { BrowserRouter } from 'react-router-dom'
 import './styles.css'
 
 import { JournalApp } from './JournalApp'
+import { Provider } from 'react-redux'
+import { store } from './store'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
